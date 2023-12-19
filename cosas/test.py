69519cec1ec5b0f644053c8a6a492dd04b3fffe1
RodrigoMartinez <77554011+Rodr1goMart1nez@users.noreply.github.com>
2023-12-18 17:55:49 -0800

    Update test.py

--- cosas/test.py
+++ cosas/test.py
@@ -20,3 +20,18 @@ for i in range(len(mails)):
 for i in range(len(mails)):
     #PROCESO DE ENVIO
     print(f"Se envio el monto {monto[i]}, al cliente {nombre[i]}, con el comercio {comercio[i]} y el numero de tarjeta: {tarjeta[i]}")
+
+
+import pyodbc
+
+conn = pyodbc.connect('Driver={SQL Server};'
+                      'Server=DESKTOP-JQUFFIT;'
+                      'Database=Pruebas;'
+                      'Trusted_Connection=yes;')
+
+cuatro = [4]
+
+cursor = conn.cursor()
+cursor.execute(f"UPDATE Tabla_PruebaUno SET ESTADO = 'ENVIADO' WHERE id_producto = {cuatro[0]}")
+
+conn.commit()
